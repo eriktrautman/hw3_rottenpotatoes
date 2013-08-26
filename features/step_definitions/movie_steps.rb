05dc07c9c890f9adb6938bfd5810515f00a6d35a
ERIK TRAUTMAN <erik.trautman@gmail.com>
2013-08-25 22:52:30 -0700

    finish scenario to restrict movies to PG and R

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -4,8 +4,12 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    Movie.create(
+        :title => movie['title'], 
+        :rating => movie['rating'], 
+        :release_date => movie['release_date'])
   end
-  flunk "Unimplemented"
+  # flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
@@ -21,8 +25,39 @@ end
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
 
-When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
+When /I (un)?check the following ratings: (.*)/ do |is_uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  # puts "\n\n\n UNCHECK: #{uncheck}! \n RATINGS: #{rating_list}!\n"
+  rating_list.split(/, /).each do |rating|
+    # puts "\n\n\n RATING IS: #{rating}!\n\n"
+    if is_uncheck == "un"
+      uncheck "ratings_#{rating}"
+    else
+      # debugger
+      check "ratings_#{rating}"
+    end
+  end
 end
+
+
+When /^I press submit$/ do
+  click_button("ratings_submit")
+end
+
+Then /^I should (not )?see movies rated: (.*)/ do |qualifier, ratings|
+  # puts "\n QUALIFIER: #{qualifier}! \n RATINGS: #{ratings}!\n\n"
+  ratings.split(/, /).each do |rating|
+      movie = Movie.where(:rating => rating).first
+      movie ? title = movie.title : title = "N/A"
+    step "I should #{qualifier}see \"#{title}\""
+  end
+end
+
+
+
+
+
+
+
